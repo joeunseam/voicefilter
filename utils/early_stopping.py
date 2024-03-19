95a358ccc5b497cbedb6e149c62198951371f4d6
JoEunSaem <88969799+joeunseam@users.noreply.github.com>
2024-03-19 09:06:13 +0900

    Create early_stopping.py

---
+++ utils/early_stopping.py
@@ -0,0 +1,43 @@
+import os
+import torch
+
+
+class EarlyStopping:
+  """Early stops the training if validation loss doesn't improve after a given patience."""
+  def __init__(self, patience=10, delta=0.00001):
+    self.patience = patience
+    self.counter = 0
+    self.delta = delta
+
+    self.val_loss_min = None
+
+
+  def check_and_save(self, new_validation_loss, model):
+    early_stop = False
+    num = 0
+
+    if self.val_loss_min is None:
+      self.val_loss_min = new_validation_loss
+      message = f'Early stopping is stated!'
+      num = 1
+    elif new_validation_loss < self.val_loss_min - self.delta:
+      message = f'V_loss decreased ({self.val_loss_min:7.5f} --> {new_validation_loss:7.5f}). Saving model...'
+      self.save_checkpoint(new_validation_loss, model)
+      self.val_loss_min = new_validation_loss
+      self.counter = 0
+      num = 2
+    else:
+      self.counter += 1
+      message = f'Early stopping counter: {self.counter} out of {self.patience}'
+      if self.counter >= self.patience:
+        early_stop = True
+        message += " *** TRAIN EARLY STOPPED! ***"
+      num = 3
+
+    return message, early_stop, num
+
+  def save_checkpoint(self, val_loss, model):
+    '''Saves model when validation loss decrease.'''
+    #torch.save(model.state_dict(), self.file_path)
+    #torch.save(model.state_dict(), self.latest_file_path)
+    self.val_loss_min = val_loss
